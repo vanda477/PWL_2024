9810b731d0ff9847115a77f633b99c54bc98dbab
Sheva Ananda Arifiansyah <arifiansyahshevaananda@gmail.com>
2024-09-06 09:30:37 +0700

    Jobsheet 2 - praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+// Route::get('/world', function () {
+//     return 'World';
+// });
+
+// Route::get('/world', function () {
+//     return 'Selamat datang';
+// });
+
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya'.$name;
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function
+// ($postId, $commentId) {
+//  return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+// });
+
+// Route::get('/user/{name?}', function ($name=Sheva) {
+//     return 'Nama saya '.$name;
+// });
+   
